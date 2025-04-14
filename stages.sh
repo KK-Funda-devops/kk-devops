fe3f97a5a9b2af85caf13d36c528a2c0bdc14989
LokeshReddy963 <lokeshreddyalla735@gmail.com>
2025-04-14 12:21:10 +0530

    updated

--- stages.sh
+++ stages.sh
@@ -3,3 +3,4 @@ fetching purpose
 pulling purpose
 hi this for pull request for i am updating the file
 hi this is ssh url purpose updated.
+hi this update is after deleting the remote we can perform.
